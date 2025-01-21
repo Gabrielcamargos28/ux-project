0f254d92a2a3d284a4c159d7993cd42fb90ac7d1
gabrielcamargos <gabrielcamargosptc@gmail.com>
2025-01-21 01:12:25 -0300

    att

--- src/main/java/com/ux_project/conclusion/domain/categoria/Categoria.java
+++ src/main/java/com/ux_project/conclusion/domain/categoria/Categoria.java
@@ -18,4 +18,20 @@ public class Categoria {
     private Long id;
 
     private String nome;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
 }
